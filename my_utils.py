6e7f24787b837d64e9729766b86e3dbb3e4776ab
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-09-28 13:53:13 -0600

    finished assignment 3 changes

--- my_utils.py
+++ my_utils.py
@@ -1,17 +1,63 @@
-# get_column("Agrofood_co2_emission.csv",0,"South Africa",1)
-
 def get_column(file_name, query_column, query_value, result_column):
-    result_array = []
-    
-    with open(file_name, 'r') as file:
-        for line in file:
-            line = line.strip()  # Remove leading/trailing whitespace
-            
-            line_array = line.split(",") # Split the line into an array
+    """
+    Extracts specifiec data of interest from CSV file.
 
-            # Check if query_column value matches query_value
-            if line_array[query_column] == query_value:
-                result_array.append(line_array[result_column])
-    
+    Args:
+        file_name (str): The name of the CSV file to read.
+        query_column (int): The index of column to check for the query_value.
+        query_value (str): The value to match in the specified query_column.
+        result_column (int): The index of column from which to extract data.
+
+    Returns:
+        list: A list of ints extracted from the result_column of matching rows.
+
+    Raises:
+        ValueError: If theres an issue converting an array into a float or int.
+        FileNotFoundError: If the specified file_name does not exist.
+
+    Example:
+        If a CSV file 'Agrofood_co2_emission.csv' has the following content:
+        ```
+        Country, Fires, Population
+        Jamaica, 25, 92.5
+        South Africa, 30, 88.0
+        Mozambique, 22, 95.5
+        ```
+
+        To extract the 'Fires' from 'Country' equal to 'South Africa',
+        you can use this function as follows:
+        ```
+        result = get_column('Agrofood_co2_emission.csv', 0, 'South Africa', 1)
+        # result will be [30]
+        ```
+    """
+    result_array = []
+
+    try:
+        with open(file_name, 'r') as file:
+            for line in file:
+                line = line.strip()
+                line_array = line.split(",")
+
+                # Check if query_column value matches query_value
+                if len(line_array) > query_column and \
+                line_array[query_column] == query_value:
+                    try:
+                        fire_num = float(line_array[result_column])
+                        fire_int = int(fire_num)
+                        result_array.append(fire_int)
+                    except ValueError:
+                        print(f"messed up converting \
+                            '{line_array[result_column]}' to float or int")
+
+    except FileNotFoundError:
+        print(f"Couldn't find the file '{file_name}'.")
+
     return result_array
 
+def main():
+    result = get_column('Agrofood_co2_emission.csv', 0, 'South Africa', 1)
+    print(result)
+
+if __name__ == '__main__':
+    main()
--- run.sh
+++ run.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-python3 print_fires.py
+
+set -e 
+set -u 
+set -o pipefail
+
+echo "... running print_fires.py ..."
+python3 print_fires.py \
+	--file_name Agrofood_co2_emission.csv \
+	--country_column 0 \
+	--country "South Africa" \
+	--fires_column 4
+
+set +e
+echo "... running print_fires.py (should give error) ..."
+python3 print_fires.py \
+	--file_name Agrofood_co2_emission.csv \
+	--country_column 0 \
+	--country "South Africa" \
+	--fires_column th
+
+echo "... running print_fires.py (should give error) ..."
+python3 print_fires.py \
+	--file_name nofile.csv \
+	--country_column 0 \
+	--country "South Africa" \
+	--fires_column 4
+set -e
