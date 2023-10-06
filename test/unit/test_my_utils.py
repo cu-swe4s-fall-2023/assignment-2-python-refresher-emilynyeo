eba88e9042e718fbf419e1207b33ebfd9f961ab2
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-10-05 19:41:33 -0600

    forgot pep8

--- test/unit/test_my_utils.py
+++ test/unit/test_my_utils.py
@@ -1,7 +1,3 @@
-# To test this file: python -m unittest test_my_utils.py
-sys import
-sys.path.insert(0, 'ls ../../src') #noqa
-
 import unittest
 import statistics
 import math
@@ -9,6 +5,12 @@ import os
 import random
 import numpy as np
 from my_utils import *
+sys import
+
+sys.path.insert(0, 'ls ../../src') #noqa
+
+#  To test this file: python -m unittest test_my_utils.py
+
 
 class TestMyUtils(unittest.TestCase):
 
@@ -35,7 +37,7 @@ class TestMyUtils(unittest.TestCase):
         self.assertEqual(get_column("wrong_name", 2, "SA", 3), None)
 
     def test_median(self):
-        result = [] 
+        result = []
         for i in range(10):
             random_integer = random.randrange(1, 10)
             result.append(random_integer)
@@ -44,19 +46,20 @@ class TestMyUtils(unittest.TestCase):
 
     def test_mean(self):
         result = []
-        for i in range(10):       
-            random_integer = random.randrange(1,10)
+        for i in range(10):
+            random_integer = random.randrange(1, 10)
             result.append(random_integer)
         self.assertEqual(mean(result), statistics.mean(result))
         self.assertEqual(mean([]), None)
 
     def test_std_dev(self):
         result = []
-        for i in range(10):  
-            random_integer = random.randrange(1,10)
+        for i in range(10):
+            random_integer = random.randrange(1, 10)
             result.append(random_integer)
         self.assertTrue(abs(std_dev(result) - statistics.stdev(result)) <= 0.5)
         self.assertEqual(std_dev([]), None)
+
 
 if __name__ == '__main__':
     unittest.main()
