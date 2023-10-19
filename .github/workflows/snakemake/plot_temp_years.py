a946e80e99ed159afc5a70100f92f631cb6adc25
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-10-18 23:26:49 -0600

    individual works but not rule all

--- .github/workflows/snakemake/plot_temp_years.py
+++ .github/workflows/snakemake/plot_temp_years.py
@@ -1,31 +1,36 @@
 import pandas as pd
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-# Defining the columns to read
+data_file = sys.argv[1]
+out_file = sys.argv[2]
+country = sys.argv[3]
+
+# columns to read
 usecols = ["Area", "Year", "Rural population", 
 			"Urban population","Food Household Consumption", 
 			"total_emission", "Average Temperature °C"]
 
-# Read the CSV file
-data = pd.read_csv("Agro2_co2_emissions.csv", usecols = usecols)
+# Read as CSV file
+data = pd.read_csv(data_file, usecols = usecols)
 
-country = "Italy"
-
-# Filter the data for the specific "Area"
+# Filter for "Area" and extract columns
 filtered_data = data[data["Area"] == country]
-
-# Extract the "Year" and "Average Temperature (°C)" columns for the plot
 years = filtered_data["Year"]
 temperature = filtered_data["Average Temperature °C"]
 
-# Create a line plot (you can also use scatter, bar, etc.)
-plt.figure(figsize=(10, 6))  # Adjust the figure size as needed
+# line plot
+plt.figure(figsize=(10, 6))
 plt.plot(years, temperature, marker='o', linestyle='-')
 plt.title(f"Average Temperature in {country}")
 plt.xlabel("Year")
 plt.ylabel("Average Temperature (°C)")
 plt.grid(True)
 
-# Show the plot or save it to a file
-plt.show()
-plt.savefig('plot.txt', format='txt')
+# Save plot as PNG
+#plt.savefig(f'{country}_ty.png', format='png')
+plt.savefig(out_file, format='png',bbox_inches='tight')
+plt.close()
