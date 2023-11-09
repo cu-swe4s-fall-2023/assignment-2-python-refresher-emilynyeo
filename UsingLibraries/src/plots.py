6d8184691f72e0f8319aaa44b3d77b51cad92e16
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-11-09 15:30:08 -0700

    that was ok actually

--- UsingLibraries/src/plots.py
+++ UsingLibraries/src/plots.py
@@ -1,8 +1,5 @@
 import sys
-import seaborn as sns
 import pandas as pd
-import matplotlib
-# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 # read in the created datafile
@@ -10,10 +7,10 @@ sys.path.insert(0, '../')  # noqa
 agro_gdp = pd.read_csv('data/co2_gdp_north.csv', sep='\t')
 
 # Set your panel
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 10))
 
 # Line plot of Temperature change over the Years
-colors = {'United States of America': 'blue', 'Mexico': 'orange', 'Canada': 'green', 'Guatemala': 'red'}
+colors = {'United States of America': 'blue', 'Mexico': 'orange', 'Canada': 'green', 'Guatemala': 'red', 'United States': 'purple'}
 for country in agro_gdp['Area'].unique():
     country_data = agro_gdp[agro_gdp['Area'] == country]
     ax1.plot(country_data['Year'], 
@@ -29,16 +26,42 @@ for country in agro_gdp['Area'].unique():
     	label=country, 
     	color=colors[country])
 
+# Scatter plot of GDP vs Total Emissions
+for country in agro_gdp['Area'].unique():
+    country_data = agro_gdp[agro_gdp['Area'] == country]
+    ax3.scatter(country_data['United States_GDP'], 
+    	country_data['total_emission'], 
+    	label=country, 
+    	color=colors[country])
+
+# Line plot of total population change over the Years
+for country in agro_gdp['Area'].unique():
+    country_data = agro_gdp[agro_gdp['Area'] == country]
+    ax4.plot(country_data['Year'], 
+        country_data['total_population'], 
+        label=country, 
+        color=colors[country])
+
 # Set titles and labels for all the plots 
-ax1.set_title('Temperature Change Over the Years')
+ax1.set_title('A. Temperature Change Over the Years')
 ax1.set_xlabel('Year')
 ax1.set_ylabel('Average Temperature °C')
 ax1.legend()
 
-ax2.set_title('Total Emissions Over the Years')
+ax2.set_title('B. Total Emissions Over the Years')
 ax2.set_xlabel('Year')
 ax2.set_ylabel('Total Emissions')
 ax2.legend()
+
+ax3.set_title('C. GDP vs Total Emissions')
+ax3.set_xlabel('GDP (United States)')
+ax3.set_ylabel('Total Emissions')
+ax3.legend()
+
+ax4.set_title('D. Total Population over the Years')
+ax4.set_xlabel('Year')
+ax4.set_ylabel('Total Population')
+ax4.legend()
 
 # Remove top and right borders
 ax1.spines['top'].set_visible(False)
@@ -47,18 +70,12 @@ ax1.spines['right'].set_visible(False)
 ax2.spines['top'].set_visible(False)
 ax2.spines['right'].set_visible(False)
 
+ax3.spines['top'].set_visible(False)
+ax3.spines['right'].set_visible(False)
+
+ax4.spines['top'].set_visible(False)
+ax4.spines['right'].set_visible(False)
+
 # Adjust layout and save the figure
 plt.tight_layout()
-plt.savefig('output_plots.png')
-plt.show()
-
-# Plot B: Scatter plot of Year vs Emissions
-
-
-# Plot C: Scatter plot of GDP vs Emissions
-
-
-# Plot D: Line plot of Year, Emissions, Temp and Population
-
-
-# Create 4 panel output file
+plt.savefig('data/output_plots.png')
