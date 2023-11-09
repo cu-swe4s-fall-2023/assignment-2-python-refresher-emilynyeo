a0714c16f804d73257f5cd551a6c5dbd7c0cf6d5
Emily <emily@cu-genvpn-comp-10.180.45.85.int.colorado.edu>
2023-11-08 22:57:10 -0700

    Sttarttting assignment9 ay too late

---
+++ UsingLibraries/src/plots.py
@@ -0,0 +1,21 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+# read in the created datafile
+co2_gdp_n = pd.read_csv('../data/co2_gdp_north.csv')
+
+# Plot A: Line plot of Year vs Temp
+
+
+# Plot B: Scatter plot of Year vs Emissions
+
+
+# Plot C: Scatter plot of GDP vs Emissions
+
+
+# Plot D: Line plot of Year, Emissions, Temp and Population
+
+
+# Create 4 panel output file
